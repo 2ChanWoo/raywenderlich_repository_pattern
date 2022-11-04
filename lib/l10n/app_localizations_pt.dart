81a4335a9cc3e668bc376f9ff089f00916650fe3
LCW <2chanwoo@groundk.com>
2022-11-04 17:25:27 +0900

    init

---
+++ lib/l10n/app_localizations_pt.dart
@@ -0,0 +1,15 @@
+
+
+
+import 'app_localizations.dart';
+
+/// The translations for Portuguese (`pt`).
+class AppLocalizationsPt extends AppLocalizations {
+  AppLocalizationsPt([String locale = 'pt']) : super(locale);
+
+  @override
+  String get quotesBottomNavigationBarItemLabel => 'Frases';
+
+  @override
+  String get profileBottomNavigationBarItemLabel => 'Perfil';
+}
